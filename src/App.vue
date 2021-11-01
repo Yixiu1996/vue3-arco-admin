1bc43f9079be1be3a32a0fbb39502084e5e9e0b2
Yixiu1996 <jczcycy@163.com>
2021-11-01 22:24:46 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <a-space>
+    <a-button type="primary">按钮</a-button>
+  </a-space>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
